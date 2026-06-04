8e6ec8ac7fc2252702f0e935fc213782232d76bf
jan20mm10 <jan20mm10@gmail.com>
2026-06-04 00:43:24 -0400

    Update logo image import path in Onboarding component

--- src/app/components/Onboarding.tsx
+++ src/app/components/Onboarding.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
+import logoImage from "../../imports/ChatGPT_Image_Apr_22,_2026,_12_46_44_AM.png";
 import { ChevronRight, ChevronLeft, ChevronDown, ChevronUp } from 'lucide-react';
-import logoImage from "figma:asset/2adf89cd05eab827313c4a2ea4459c75b1ff962c.png";
 import { WarmthPass } from './WarmthOverlay';
 
 interface OnboardingProps {
